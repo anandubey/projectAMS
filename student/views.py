678fe0d98a8aca7f779c35180a4a8662fc77b3c8
ByteBaker <shraddha.kishan@gmail.com>
2019-07-12 21:23:44 +0530

    db fixed

--- student/views.py
+++ student/views.py
@@ -17,20 +17,23 @@ def student(request):
             this_semester += 1
         print('this sem', this_semester)
         this_year = str(date.today().year)
-        courses = Semester_wise_course.objects.get(department=department, semester=this_semester, year=this_year).courses.split('-')
-        att_data_list = []                                         # using list of dictionaries to store attendance 
-        for course_code in courses:
-            total = attendance.objects.filter(reg_no=user_instance, course_code=course_code).count()
-            present = attendance.objects.filter(reg_no=user_instance, course_code=course_code, attendance='P').count()
-            if total != 0:
-                percent_present = (present/total)*100
-            else:
-                percent_present = 0.0
-            less_attendance = False
-            if percent_present < 75:
-                less_attendance = True
-            att_data = {'course_code':course_code, 'total_classes':total, 'attended':present, 'percent':percent_present, 'less_attend':less_attendance}
-            att_data_list.append(att_data)
+        try:
+            courses = Semester_wise_course.objects.get(department=department, semester=this_semester, year=this_year).courses.split('-')
+            att_data_list = []                                         # using list of dictionaries to store attendance 
+            for course_code in courses:
+                total = attendance.objects.filter(reg_no=user_instance, course_code=course_code).count()
+                present = attendance.objects.filter(reg_no=user_instance, course_code=course_code, attendance='P').count()
+                if total != 0:
+                    percent_present = (present/total)*100
+                else:
+                    percent_present = 0.0
+                less_attendance = False
+                if percent_present < 75:
+                    less_attendance = True
+                att_data = {'course_code':course_code, 'total_classes':total, 'attended':present, 'percent':percent_present, 'less_attend':less_attendance}
+                att_data_list.append(att_data)
+        except Semester_wise_course.DoesNotExist:
+            att_data_list = []
         return render(request, 'student/student-home.html', {'user_instance': user_instance, 'att_data_list': att_data_list, 'stu_sem':this_semester })
     else:
         return redirect('home')
